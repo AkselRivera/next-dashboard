82fbf2771e0089ed61f31ab7a5a831ebfee94e84
Aksel Rivera <>
2023-10-31 22:26:15 -0600

    [Feature] (Layout): Added new layout for (general) pages

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,67 @@
+import Image from 'next/image'
+
+import { TbBrandNextjs } from 'react-icons/tb'
+import { LuLayoutDashboard } from 'react-icons/lu'
+import { AiOutlineCalculator } from 'react-icons/ai'
+import { SidebarMenuItem } from './SidebarMenuItem'
+
+const MenuItems = [
+  {
+    path: '/dashboard/main',
+    icon: <LuLayoutDashboard className="mr-2" size={25} />,
+    title: 'Dashboard',
+    subtitle: 'Data Overview',
+  },
+  {
+    path: '/dashboard/counter',
+    icon: <AiOutlineCalculator className="mr-2" size={25} />,
+    title: 'Counter',
+    subtitle: 'App state',
+  },
+]
+
+export function Sidebar() {
+  return (
+    <div
+      id="menu"
+      className="bg-gray-900 min-h-screen z-10 text-slate-300 min-w-[18rem]  left-0 h-screen overflow-y-scroll"
+    >
+      <div id="logo" className="my-4 px-4">
+        <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
+          <TbBrandNextjs className="mr-2" />
+          <span>Dash</span>
+          <span className="text-blue-500">8</span>.
+        </h1>
+        <p className="text-slate-500 text-sm">
+          Manage your actions and activities
+        </p>
+      </div>
+      <div id="profile" className="px-4 py-10">
+        <p className="text-slate-500">Welcome back,</p>
+        <a href="#" className="inline-flex space-x-2 items-center">
+          <span>
+            <Image
+              width={50}
+              height={50}
+              src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c"
+              alt="avatar"
+              className="rounded-full w-8 h-8"
+            />
+          </span>
+          <span className="text-sm md:text-base font-bold">Aksel Rivera</span>
+        </a>
+      </div>
+      <div id="nav" className="w-full px-4">
+        {MenuItems.map((item) => (
+          <SidebarMenuItem
+            key={item.path}
+            path={item.path}
+            icon={item.icon}
+            title={item.title}
+            subtitle={item.subtitle}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,15 @@
+import { CardCounter } from '@/app/shopping-cart'
+
+export const metadata = {
+  title: 'Counter Page',
+  description: 'Counter page description',
+}
+
+export default function CounterPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CardCounter value={20} />
+    </div>
+  )
+}
